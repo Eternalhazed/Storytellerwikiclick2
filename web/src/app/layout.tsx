425327240f2f4808e157b668d75374bef56aa9c2
Shane Friedman <smoores-gpg@friedmans.us>
2024-12-19 19:07:55 -0500

    Install and configure mantine and tailwind

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,18 +1,33 @@
+import { Inter, Young_Serif } from "next/font/google"
+
 import { ApiHostContextProvider } from "@/contexts/ApiHostContext"
 import "./globals.css"
 import { proxyRootPath } from "./apiHost"
 // import { Header } from "@/components/layout/Header"
-import styles from "./layout.module.css"
-import { Sidebar } from "@/components/layout/Sidebar"
 import {
   EMPTY_PERMISSIONS as EMPTY_PERMISSIONS,
   UserPermissionsProvider,
 } from "@/contexts/UserPermissions"
 import { createAuthedApiClient } from "@/authedApiClient"
 import { User } from "@/apiModels"
-import { Header } from "@/components/layout/Header"
 import { ApiClientError } from "@/apiClient"
 import { logger } from "@/logging"
+import { AppShell } from "@/components/AppShell"
+import { ColorSchemeScript } from "@mantine/core"
+import "@mantine/core/styles.css"
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+})
+
+const youngSerif = Young_Serif({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+  variable: "--font-young-serif",
+})
 
 export const metadata = {
   title: "Storyteller",
@@ -36,17 +51,20 @@ export default async function RootLayout({
   }
 
   return (
-    <html lang="en">
-      <body>
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={inter.className + " " + youngSerif.className}
+    >
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body suppressHydrationWarning>
         <ApiHostContextProvider value={{ rootPath: proxyRootPath }}>
           <UserPermissionsProvider
             value={currentUser?.permissions ?? EMPTY_PERMISSIONS}
           >
-            <Header version={version} />
-            <div className={styles["container"]}>
-              <Sidebar className={styles["sidebar"]} version={version} />
-              {children}
-            </div>
+            <AppShell version={version}>{children}</AppShell>
           </UserPermissionsProvider>
         </ApiHostContextProvider>
       </body>
---
+++ web/src/components/AppShell.tsx
@@ -0,0 +1,113 @@
+"use client"
+
+import NextLink from "next/link"
+import {
+  MantineProvider,
+  AppShell as MantineAppShell,
+  AppShellHeader,
+  AppShellNavbar,
+  AppShellMain,
+  createTheme,
+  MantineColorsTuple,
+  Burger,
+  NavLink,
+  Title,
+  Anchor,
+  Group,
+} from "@mantine/core"
+import { ReactNode } from "react"
+import { useDisclosure } from "@mantine/hooks"
+import { usePathname } from "next/navigation"
+
+const stOrange: MantineColorsTuple = [
+  "#fff1e7",
+  "#fbe2d3",
+  "#f6c2a5",
+  "#f1a173",
+  "#ed8449",
+  "#eb722f",
+  "#ea6920",
+  "#d15815",
+  "#ba4d0f",
+  "#a34106",
+]
+
+const theme = createTheme({
+  primaryColor: "st-orange",
+  colors: {
+    "st-orange": stOrange,
+  },
+})
+
+interface Props {
+  version: string
+  children: ReactNode
+}
+
+export function AppShell({ children }: Props) {
+  const [opened, { toggle }] = useDisclosure(false)
+  const pathname = usePathname()
+
+  return (
+    <MantineProvider theme={theme} defaultColorScheme="light">
+      <MantineAppShell
+        withBorder={false}
+        header={{ height: 60 }}
+        {...(pathname === "/"
+          ? undefined
+          : {
+              navbar: {
+                width: 240,
+                breakpoint: "sm",
+                collapsed: { mobile: !opened },
+              },
+            })}
+      >
+        <AppShellHeader className="text-st-orange-50 p-4">
+          <Group align="center">
+            <Burger
+              className="pb-[0.625rem]"
+              opened={opened}
+              color="white"
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
+            />
+            <Anchor component={NextLink} href="/" className="block">
+              <Title size="h3" className="font-heading text-black">
+                Storyteller
+              </Title>
+            </Anchor>
+          </Group>
+        </AppShellHeader>
+        <AppShellNavbar className="mt-8">
+          <NavLink
+            component={NextLink}
+            href="/"
+            label="Books"
+            active={pathname === "/"}
+          />
+          <NavLink
+            component={NextLink}
+            href="/users"
+            label="Users"
+            active={pathname === "/users"}
+          />
+          <NavLink
+            component={NextLink}
+            href="/settings"
+            label="Settings"
+            active={pathname === "/settings"}
+          />
+          <NavLink
+            component="a"
+            href="/logout"
+            label="Logout"
+            active={pathname === "/logout"}
+          />
+        </AppShellNavbar>
+        <AppShellMain className="mt-8">{children}</AppShellMain>
+      </MantineAppShell>
+    </MantineProvider>
+  )
+}
